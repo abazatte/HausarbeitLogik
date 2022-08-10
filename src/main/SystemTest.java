7e09e67a55fe8a114e3b9f6b87bb9cbebe7f79f8
maxono <maximilian.jaesch@hs-osnabrueck.de>
2022-08-10 14:13:32 +0200

    parser loops fixed

--- src/main/SystemTest.java
+++ src/main/SystemTest.java
@@ -1,91 +1,29 @@
 package main;
 
 import org.jpl7.PrologException;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import helper.Parser;
 
+
 public class SystemTest {
-    Parser parser;
-
-    @Before
-    public void setUp() {
-        parser = new Parser();
-    	Main.initialize();
-    }
-
-//mal, geteilt, punkt vor strich, sinus cosinus tangens  
-	@Test
-    public void additionTest() {
-        Assert.assertEquals(12.0, parser.parse("3+3+3+3"), 0.0);
-    }
+	Parser parser;
 	
-	@Test
-	public void multiplikationTest() {
-		Assert.assertEquals(81.0, parser.parse("3*3*3*3"), 0.0);
+	@Before
+	public void setup() {
+		Main.initialize();
+		parser = new Parser();
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Test
-	public void subtraktionTest() {
-		Assert.assertEquals(-6.0, parser.parse("3-3-3-3"), 0.0);
-	}
-	
-	@Test
-	public void divisionTest() {
-		Assert.assertEquals(2.0, parser.parse("10/5"), 0.0);
-	}
-	
-	@Test
-	public void punktVorStrich() {
-		Assert.assertEquals(15.0, parser.parse("3+3*3+3"), 0.0);
-	}
-	
-	@Test
-	public void klammerTest() {
-		Assert.assertEquals(36.0, parser.parse("(3+3)*(3+3)"), 0.0);
-	}
-	
-	@Test
-	public void sinusTest() {
-		Assert.assertEquals(0.0, parser.parse("sin(0)"), 0.0);
-	}
-	
-	public void sinusKlammerTest() {
-		Assert.assertEquals(0.0, parser.parse("(1-1)*(1+1))"), 0.0);
-	}
-	
-	@Test
-	public void cosinusTest() {
-		Assert.assertEquals(1.0, parser.parse("cos(0)"), 0.0);
-	}
-	
-	@Test
-	public void tangensTest() {
-		Assert.assertEquals(0.0, parser.parse("tan(0)"), 0.0);
-	}
-	
-	/*
-	@Test
-	public void maxUeberlaufTest() {
-		double d = Double.MAX_VALUE;
-		Assert.assertEquals(0.0, parser.parse(d + "+1"), 0.0);
-	}
-	
-	@Test
-	public void minUeberlaufTest() {
-		double d = Double.MIN_VALUE;
-		Assert.assertEquals(0.0, parser.parse(d + "-1"), 0.0);
-	}
-	*/
-	@Test
-	public void teilenDurchNullTest() {
+	public void additionTest() {
 		try {
-			parser.parse("1/0");
+			Assert.assertEquals(6.0, parser.parse("3.0+3.0"),0.0);
 		} catch (PrologException e) {
-			// TODO: handle exception
-			
+			System.out.println("moin exceptio");
+			e.printStackTrace();
 		}
 	}
+	
 }
