608713889bd68373bf313c19b753eaaa1f095e0b
maxono <maximilian.jaesch@hs-osnabrueck.de>
2022-08-12 13:10:06 +0200

    listener

---
+++ src/listener/CalculatorHelpMenuMouseListener.java
@@ -0,0 +1,31 @@
+package listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
+
+public class CalculatorHelpMenuMouseListener implements MouseListener{
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, " VON Werte duerfen nicht groesser als BIS Werte ");
+	}
+}
